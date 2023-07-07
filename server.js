071d50b6a99f0b255d56ebbf9b3d9b4eec1e7436
Oblivious <thanos.papap42@gmail.com>
2023-07-07 17:42:48 +0300

    Initial commit.

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const path = require("path")
+app.use("/", express.static(path.join(__dirname, "/")));
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.use(express.static("/"));
+
+app.listen(3000, () => {
+    console.log("Example app listening on port 3000");
+});
