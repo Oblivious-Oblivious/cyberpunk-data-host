d4b6f32b4a34706441b3626991299f56d44d18d7
Oblivious <thanos.papap42@gmail.com>
2023-07-08 14:27:51 +0300

    Added a route for serving json files.

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
 
+const cyberjson = require("./cyberpunk.json");
+
 const path = require("path")
 app.use("/", express.static(path.join(__dirname, "/")));
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
     res.send("Hello World!");
+});
+
+app.get("/cyberpunk", (_, res) => {
+    res.header("Content-Type", "application/json");
+    res.send(JSON.stringify(cyberjson));
 });
 
 app.use(express.static("/"));
