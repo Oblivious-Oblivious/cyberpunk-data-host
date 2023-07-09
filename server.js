76661a78d6a555f3cbcd4c56694597e25305d6c2
Oblivious <thanos.papap42@gmail.com>
2023-07-09 16:52:08 +0300

    .

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const app = express();
 const cyberjson = require("./cyberpunk.json");
 
 const path = require("path")
-//app.use("/", express.static(path.join(__dirname, "/")));
+app.use("/", express.static(path.join(__dirname, "/")));
 
 app.get("/", (_, res) => {
     res.header("Content-Type", "application/json");
     res.send(JSON.stringify(cyberjson));
 });
 
-//app.use(express.static("/"));
+app.use(express.static("/"));
 
 app.listen(3000, () => {
     console.log("Listening on 3000");
