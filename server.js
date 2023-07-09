ef7390bf09160ceb9cb48c920685e639ce513e85
Oblivious <thanos.papap42@gmail.com>
2023-07-09 11:01:05 +0300

    Routed json to homepage.

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ const path = require("path")
 app.use("/", express.static(path.join(__dirname, "/")));
 
 app.get("/", (_, res) => {
-    res.send("Hello World!");
-});
-
-app.get("/cyberpunk", (_, res) => {
     res.header("Content-Type", "application/json");
     res.send(JSON.stringify(cyberjson));
 });
