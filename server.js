750367548a6af390653fac477f73bc627d6f8d83
Oblivious <thanos.papap42@gmail.com>
2023-08-25 13:42:54 +0300

    Added use route for static images.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cyberjson = require("./cyberpunk.json");
 
 const path = require("path")
 app.use("/", express.static(path.join(__dirname, "/")));
+app.use("/", express.static(path.join(__dirname, "/images")));
 
 app.get("/", (_, res) => {
     res.header("Content-Type", "application/json");
